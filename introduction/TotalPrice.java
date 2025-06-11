e14ace05d8073b18b475fb6b41347c9cd7894f01
Thibaud-LAMON <thib.lamon@gmail.com>
2025-06-11 12:19:51 +0200

    fixing typo and deleting original comment

--- introduction/TotalPrice.java
+++ introduction/TotalPrice.java
@@ -1,24 +1,18 @@
 public class TotalPrice {
     public static void bulk(double total){
-
         if (total > 1000) {
-            // bulk order discount
             total -= 50;
         }
     }
 
     public static void large(Order order, double total){
-
         if (order.getItems().size() > 10) {
-            // discount for large number of items
             total -= 20;
         }
     }
 
-    public static void international(Order order,double total){
-
+    public static void international(Order order, double total){
         if (order.isInternational()) {
-            // international shipping fee
             total += 100;
         }
     }
