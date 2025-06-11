a92485896165bb795ee53422e4850e326b912c92
Bastienlsg <bastien.lassaigne@gmail.com>
2025-06-11 11:57:14 +0200

    refacto: add constant class, and rebuild of the logic

---
+++ introduction/OrderConstants.java
@@ -0,0 +1,14 @@
+package introduction;
+
+public final class OrderConstants {
+    private OrderConstants() {}
+
+    public static final double PREMIUM_DISCOUNT = 0.9;
+    public static final double HOLIDAY_DISCOUNT = 0.8;
+    public static final double BULK_ORDER_THRESHOLD = 1000.0;
+    public static final double BULK_ORDER_DISCOUNT = 50.0;
+    public static final int LARGE_ORDER_THRESHOLD = 10;
+    public static final double LARGE_ORDER_DISCOUNT = 20.0;
+    public static final double INTERNATIONAL_SHIPPING_FEE = 100.0;
+    public static final String HOLIDAY_DISCOUNT_CODE = "HOLIDAY";
+}
