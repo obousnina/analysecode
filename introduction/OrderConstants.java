e3c8f9d7625227d9232b08ade41cbfb7da52858e
DamienML <morice-legouet.e2103219@etud.univ-ubs.fr>
2025-06-11 12:15:04 +0200

    correction

--- introduction/OrderConstants.java
+++
@@ -1,9 +0,0 @@
-public class OrderConstants {
-    public static final double PREMIUM_DISCOUNT = 0.10;
-    public static final double HOLIDAY_DISCOUNT = 0.20;
-    public static final String HOLIDAY_CODE = "HOLIDAY";
-    public static final double BULK_DISCOUNT = 50.0;
-    public static final double LARGE_ITEM_DISCOUNT = 20.0;
-    public static final int LARGE_ITEM_THRESHOLD = 10;
-    public static final double INTERNATIONAL_SHIPPING_FEE = 100.0;
-}
