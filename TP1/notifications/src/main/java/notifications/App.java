879399b749dfd96af69223ce35e4c708b391066e
DamienML <morice-legouet.e2103219@etud.univ-ubs.fr>
2025-06-11 15:03:05 +0200

    move App.java

---
+++ TP1/notifications/src/main/java/notifications/App.java
@@ -0,0 +1,16 @@
+package notifications;
+
+import notifications.model.Utilisateur;
+import notifications.service.NotificationManager;
+
+public class App {
+    public static void main(String[] args) {
+        NotificationManager manager = new NotificationManager();
+        Utilisateur utilisateur = new Utilisateur(
+                "utilisateur@example.com",
+                "+33612345678",
+                "device-token-123");
+
+        manager.envoyerNotification(utilisateur, "Test de notification");
+    }
+}
