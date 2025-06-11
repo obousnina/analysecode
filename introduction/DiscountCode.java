26a3a6f82e481ff41473dde3c1a4ac46989e12aa
DroVz <alexrichard1@live.fr>
2025-06-11 12:00:13 +0200

    feature/Alex: refactor OrderProcessor

--- introduction/DiscountCode.java
+++ introduction/DiscountCode.java
@@ -1,3 +1,5 @@
-public class DiscountCode {
+public class DiscountConstant {
     public static final String HOLIDAY_CODE = "HOLIDAY";
+    public static final double PREMIUM_DISCOUNT = 0.9;
+    public static final double HOLIDAY_DISCOUNT = 0.8;
 }
--- introduction/OrderProcessor.java
+++ introduction/OrderProcessor.java
@@ -2,8 +2,6 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class OrderProcessor {
 
-    private static final double PREMIUM_DISCOUNT = 0.9;
-    private static final double HOLIDAY_DISCOUNT = 0.8;
     public double processOrder(Order order, boolean isPremiumCustomer, String discountCode) {
         AtomicReference<Double> total = new AtomicReference<>((double) 0);
         order.getItems().foreach((item) -> total.updateAndGet(price -> new Double((double) (price + getPrice(item, isPremiumCustomer, discountCode)))));
@@ -12,13 +10,11 @@ public class OrderProcessor {
 
     private double getPrice(OrderItem item, boolean isPremiumCustomer, String discountCode) {
         double price = item.getQuantity() * item.getUnitPrice();
-        // 10% discount for premium customers
         if (isPremiumCustomer) {
-            price *= PREMIUM_DISCOUNT;
+            price *= DiscountConstant.PREMIUM_DISCOUNT;
         }
-        // 20% holiday discount
-        if (discountCode != null && discountCode.equals(DiscountCode.HOLIDAY_CODE)) {
-            price *= HOLIDAY_DISCOUNT;
+        if (discountCode != null && discountCode.equals(DiscountConstant.HOLIDAY_CODE)) {
+            price *= DiscountConstant.HOLIDAY_DISCOUNT;
         }
         return price;
     }
@@ -26,15 +22,12 @@ public class OrderProcessor {
     private double getRealPrice(double price, Order order) {
         var newPrice = price;
         if (price > Constant.BULK_ORDER) {
-            // bulk order discount
             newPrice -= 50;
         }
         if (order.getItems().size() > Constant.LARGE_ITEM) {
-            // discount for large number of items
             newPrice -= 20;
         }
         if (order.isInternational()) {
-            // international shipping fee
             newPrice += 100;
         }
         return newPrice;
