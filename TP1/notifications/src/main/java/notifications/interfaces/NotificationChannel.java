e6058bf084bd8353e586e158f5bd3f63abaef370
gwendoudou50 <gwendal.aube@icloud.com>
2025-06-11 15:06:51 +0200

    [A] Added documentation of NotificationChannel.java

--- TP1/notifications/src/main/java/notifications/interfaces/NotificationChannel.java
+++ TP1/notifications/src/main/java/notifications/interfaces/NotificationChannel.java
@@ -1,6 +1,18 @@
 package notifications.interfaces;
 
+/**
+ * Interface to implement the send of a message.
+ */
 public interface NotificationChannel {
-    void envoyer(String destinataire, String message);
-    boolean peutEnvoyer(String destinataire);
+    /**
+     * @param target  which is a String that define who receive the message.
+     * @param message which is a String that is the content of the message.
+     */
+    void send(String target, String message);
+
+    /**
+     * @param target which is a String that define who receive the message.
+     * @return a boolean to know if you can send a message to this target.
+     */
+    boolean canSend(String target);
 } 
