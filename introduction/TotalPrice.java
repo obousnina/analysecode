69db0e2e344bee9de510759273c57b232ad82b0c
Thibaud-LAMON <thib.lamon@gmail.com>
2025-06-11 12:02:43 +0200

    idea for the last three conditions

---
+++ introduction/TotalPrice.java
@@ -0,0 +1,7 @@
+public class TotalPrice {
+    /**
+     * L'idée est de retravailler la logique des trois dernières conditions :
+     * on en fait des méthodes avant de les appeler dans la classe
+     * OrderProcessor
+     */
+}
