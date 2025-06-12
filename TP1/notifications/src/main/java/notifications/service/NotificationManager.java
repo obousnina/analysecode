6782a8d0d4579081aba31970b052cf5a1b201fbd
gwendoudou50 <gwendal.aube@icloud.com>
2025-06-12 09:32:31 +0200

    [A] Added documentation of NotificationManager.java

--- TP1/notifications/src/main/java/notifications/service/NotificationManager.java
+++ TP1/notifications/src/main/java/notifications/service/NotificationManager.java
@@ -9,6 +9,9 @@ import notifications.channels.SMSChannel;
 import notifications.interfaces.NotificationChannel;
 import notifications.model.User;
 
+/**
+ * Class to manage the send of notifications.
+ */
 public class NotificationManager {
     private final List<NotificationChannel> channels;
 
@@ -19,13 +22,24 @@ public class NotificationManager {
         this.channels.add(new PushChannel());
     }
 
+    /**
+     * Add a the channel to the notification manager.
+     *
+     * @param channel is a NotificationChannel which correspond to a channel like email, sms or push notification.
+     */
     public void addChannel(NotificationChannel channel) {
         this.channels.add(channel);
     }
 
+    /**
+     * Send a notification to the user. The content of the notification is the content of the message.
+     *
+     * @param user    is a User who will receive the message
+     * @param message is a String which correspond to the content of the notification to send.
+     */
     public void sendNotification(User user, String message) {
         for (NotificationChannel channel : channels) {
-            channel.send(user, message); 
+            channel.send(user, message);
         }
     }
 }
